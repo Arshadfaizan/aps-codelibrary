efa860329b769c20ce6b891041c16a06f6476019
Arshad Faizan <47129348+Arshadfaizan@users.noreply.github.com>
2021-06-20 13:43:56 +0530

    Update 2_dfs.c

--- 2_dfs.c
+++ 2_dfs.c
@@ -1,39 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void dfs(int);
-int g[10][10],visited[10],n;
+struct node
+{
+    int data;
+    struct node *next;
+};
+int visited[];
+void read_graph(struct node *ad[],int no_nodes);
+void print_graph(struct node *ad[],int no_nodes);
+void dfs(struct node *ad[],int i);
 int main()
 {
-   int i,j,k;
-   printf("enter the no of vertices\n");
-   scanf("%d",&n);
-   printf(" enter the adjacency matrix of the graph\n");
-   for(i=0;i<n;i++)
-   {
-       for(j=0;j<n;j++)
-       {
-           scanf("%d",&g[i][j]);
-       }
-   }
-   for(i=0;i<n;i++)
-   {
-       visited[i]=0;
-   }
-   dfs(0);
-    return 0;
+ int i,j,k,nodes;
+ printf("enter the no of node\n");
+ scanf("%d",&nodes);
+ struct node *adj[nodes];
+ for(i=0;i<nodes;i++)
+    adj[i]=NULL;
+ read_graph(adj,nodes);
+ print_graph(adj,nodes);
+ for(i=0;i<nodes;i++)
+    visited[i]=NULL;
+ dfs(adj,0);
+ return 0;
 }
 
-void dfs(int i)
+void read_graph(struct node *ad[],int no_nodes)
 {
-    int j;
-    printf("\n%d",i);
-    visited[i]=1;
-    for(j=0;j<n;j++)
-    {
-        if(!visited[j]&&g[i][j]==1)
-            dfs(j);
-    }
+  struct node *new_node;
+  int i,j,k,val;
+  for(i=0;i<no_nodes;i++)
+  {
+      struct node *last=NULL;
+      printf("the no of the neighbour nodes of %d\n",i);
+      scanf("%d",&k);
 
+      for(j=0;j<k;j++)
+      {
+          printf("enter the value of %d neigbour of %d",j,i);
+          scanf("%d",&val);
+          new_node=(struct node *)malloc(sizeof(struct node *));
+          new_node->data=val;
+          new_node->next=NULL;
+
+          if(ad[i]==NULL)
+           {
+             ad[i]=new_node;
+           }
+          else
+           {
+             last->next=new_node;}
+            last=new_node;
+
+      }
+  }
+
+}
+
+void print_graph(struct node *ad[],int no_nodes)
+{
+ struct node *ptr=NULL;
+ int i;
+ for(i=0;i<no_nodes;i++)
+ {
+     ptr=ad[i];
+     printf("\nthe neighbour of %d are : ",i);
+     while(ptr != NULL)
+     {
+         printf("%d\t",ptr->data);
+         ptr=ptr->next;
+
+     }
+ }
+}
+void dfs(struct node *ad[],int i)
+{
+    struct node *p;
+    printf("\n%d",i);
+    p=ad[i];
+    visited[i]=1;
+    while(p!=NULL)
+    {
+        i=p->data;
+        if(!visited[i])
+        {
+            dfs(ad,i);
+        }
+            p=p->next;
+
+    }
 
 }
