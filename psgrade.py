1de14746800613107805be19df0fee25a3bd29f1
Arshad Faizan <47129348+Arshadfaizan@users.noreply.github.com>
2021-06-23 02:25:50 +0530

    Create psgrade.py

---
+++ psgrade.py
@@ -0,0 +1,10 @@
+n=int(input())
+if(1<=n<=100):
+    for _ in range(n):
+        arr=list(map(int,input().split()))
+        a=arr[4]+arr[5]+int(arr[6])
+        if(1<=arr[0],arr[1],arr[2],arr[4],arr[5],arr[6]<=100 and 1<=a,arr[3]<=300 and a<=arr[3]):
+            if(arr[4]>=arr[0] and arr[5]>=arr[1] and arr[6]>=arr[2] and int(arr[3])<=a):
+                print("YES")
+            else:    
+                print("NO")
