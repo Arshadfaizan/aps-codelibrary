1c0faeb7233cfd3081dfe4a4346e92d396f00bf3
Arshad Faizan <47129348+Arshadfaizan@users.noreply.github.com>
2021-06-24 13:31:45 +0530

    Create ContingStraincase.py

---
+++ ContingStraincase.py
@@ -0,0 +1,9 @@
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+def countWays(s):
+    return fib(s + 1)
+s = int(input())
+print "Number of ways = ",
+print countWays(s)
